43bb114af8ab065437a15b6250aff60910e8957c
I-Vulpes-I <153371980+I-Vulpes-I@users.noreply.github.com>
2024-02-06 23:22:02 +0000

    Add files via upload
    Notes taken from arrays section.

---
+++ CodeSpace - Induction - Lvl 6/JavaScript/Practise/arraysprac.js
@@ -0,0 +1,28 @@
+/*
+//  Looping through array 
+const fruits = ["banana", "orange", "mango"];
+let fLen = fruits.length;
+
+let text = "<ul>";
+for (let i = 0; i < fLen; i++) {
+  text += "<li>" + fruits[i] + "</li>";
+}
+text += "</ul>";
+
+console.log(text)
+
+const fruits = ["banana", "orange", "mango"];
+fruits.push("apple");  // Adds a new element (Apple) to fruits
+
+const fruits = ["banana", "orange", "mango"];
+fruits[fruits.length] = "apple";  // Adds a new element (Apple) to fruits
+
+const person = [];
+    
+person["firstName"] = "John";
+person["lastName"] = "Doe";
+person["age"] = 46;
+person.length;     // Will return 0
+person[0];         // Will return undefined
+
+*/
