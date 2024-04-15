19f1d2ac10e8c0568c6d66cfb6b19a702576243c
I-Vulpes-I <153371980+I-Vulpes-I@users.noreply.github.com>
2024-04-15 03:43:30 +0100

    Add files via upload
    Python file with first 2 activities

---
+++ CodeSpace - Induction - Lvl 6/Python/Python Basics/Activity2.py
@@ -0,0 +1,6 @@
+userimp1 = int(input("Input the 1st number: "))
+userimp2 = int(input("Input the 2nd number: "))
+
+sum = userimp1 // userimp2
+
+print("The divison of the first number and the second number is : ", sum)
